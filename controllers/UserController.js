2abb277d3f39be5123c3a7072a1c266074a6e6f2
Leonardo Santiago <leonardo201220@gmail.com>
2024-02-19 14:32:22 -0300

    Busca de usuários

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,9 +1,11 @@
+const { text } = require("body-parser");
 const { findEmail } = require("../models/User");
 var User = require("../models/User")
 class UserController{
 
     async index(req, res){
-
+        var users = await User.findAll();
+        res.json(users);
     }
 
     async create(req, res){
@@ -23,6 +25,19 @@ class UserController{
             await User.new(email,password,name);
             res.status(200);
             res.send("Tudo Ok!")
+    }
+
+    async findUser(req,res){
+        var id = req.params.id;
+        var user = await User.findById(id);
+        if (user == undefined) {
+            res.status(404);
+            res.json({Error: "Usuário não encontrado!"})
+            
+        } else {
+            res.status(200);
+            res.json(user);
+        }
     }
 }
 
--- models/User.js
+++ models/User.js
@@ -2,6 +2,31 @@ var knex = require("../database/connection");
 var bcrypt = require("bcrypt");
 class User{
 
+    async findAll(){
+        try {
+            var result = await knex.select(["id","name","email","role"]).table("users");
+            return result;
+        } catch (error) {
+            console.log(error);
+            return [];
+        }
+
+    }
+
+    async findById(id){
+        try {
+            var result = await knex.select(["id","name","email","role"]).where({id:id}).table("users");
+            if (result.length > 0) {
+                return result[0];
+            }else{
+                return undefined;
+            }
+        } catch (error) {
+            console.log(error);
+            return [];
+        }
+    }
+
     async new(email,password,name){
         try {
            var hash = await bcrypt.hash(password, 10);

5ac975b95359ff49aef18d239363c1da048590ce
Leonardo Santiago <leonardo201220@gmail.com>
2024-03-25 15:01:05 -0300

    Exclusão da rota Home

--- routes/routes.js
+++ routes/routes.js
@@ -1,11 +1,9 @@
 var express = require("express")
 var app = express();
 var router = express.Router();
-var HomeController = require("../controllers/HomeController");
 var UserController = require("../controllers/UserController");
 var AdminAuth = require("../middleware/AdminAuth");
 
-router.get('/', HomeController.index);
 router.post('/user',AdminAuth,UserController.create);
 router.get('/user',UserController.index);
 router.get('/user/:id',UserController.findUser);

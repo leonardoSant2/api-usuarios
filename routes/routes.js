cb5a4c85862748717c29697c9b7fe614749a28f2
Leonardo Santiago <leonardo201220@gmail.com>
2024-04-10 14:34:00 -0300

    Adaptação para o consumo da API

--- routes/routes.js
+++ routes/routes.js
@@ -5,8 +5,8 @@ var UserController = require("../controllers/UserController");
 var AdminAuth = require("../middleware/AdminAuth");
 
 router.post('/user',UserController.create);
-router.get('/user',UserController.index);
-router.get('/user/:id',UserController.findUser);
+router.get('/user',AdminAuth,UserController.index);
+router.get('/user/:id',AdminAuth,UserController.findUser);
 router.put('/user',AdminAuth,UserController.edit);
 router.delete('/user/:id',AdminAuth,UserController.remove);
 router.post('/recoverpassword',UserController.recoverPassword);
